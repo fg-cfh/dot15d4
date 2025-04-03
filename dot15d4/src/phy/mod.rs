fdd55ab3c2a5ddd17cca5026093a17fab0e46347
Florian Grandel <fgrandel@code-for-humans.de>
2025-04-03 22:57:56 +0200

    fix typos
    Signed-off-by: Florian Grandel <fgrandel@code-for-humans.de>

--- dot15d4/src/phy/mod.rs
+++ dot15d4/src/phy/mod.rs
@@ -1,7 +1,7 @@
 //! Access to IEEE 802.15.4 devices.
 //!
 //! This module provides access to IEEE 802.15.4 devices. It provides a trait
-//! for transmitting and recieving frames, [Device].
+//! for transmitting and receiving frames, [Device].
 
 pub mod config;
 pub mod constants;
